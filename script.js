c177ccfd2870ab16dad3f13a6af9c570e8cec665
Helena Adelsten <helenaltv@gmail.com>
2024-02-05 13:13:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const accordionElement = document.querySelector(".accordion");
+
+async function getPosts() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    if (!response.ok) {
+      throw new Error("Something went wrong while fetching data");
+    }
+
+    const data = await response.json();
+
+    data.slice(0, 4).forEach((post, index) => {
+      const titleElement = document.getElementById(`Question${index + 1}`);
+      titleElement.textContent = `Question ${index + 1}`;
+
+      // Lägg till en knapp i varje titel
+      const toggleBtn = document.createElement("button");
+      toggleBtn.classList.add("toggle-btn");
+      toggleBtn.textContent = "+";
+      titleElement.insertBefore(toggleBtn, titleElement.firstChild);
+
+      titleElement.addEventListener("click", () =>
+        toggleDescription(titleElement)
+      );
+
+      const descriptionElement = titleElement.nextElementSibling;
+      descriptionElement.textContent = post.body;
+    });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", getPosts);
+
+function toggleDescription(titleElement) {
+  titleElement.classList.toggle("active");
+  const descriptionElement = titleElement.nextElementSibling;
+  descriptionElement.classList.toggle("show");
+
+  const toggleBtn = titleElement.querySelector(".toggle-btn");
+  toggleBtn.textContent = toggleBtn.textContent === "+" ? "-" : "+";
+}
